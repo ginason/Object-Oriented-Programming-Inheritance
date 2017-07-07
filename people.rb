d8a06381daf3fc96b688c4b55d64554cf1333551
Gina Son <ginasunah33@gmail.com>
2017-07-07 14:05:48 -0400

    complete People.rb

---
+++ people.rb
@@ -0,0 +1,43 @@
+class Person
+  def initialize(name)
+    @name = name
+
+  end
+
+  def greeting
+    puts "Hi, my name is #{@name}"
+  end
+
+end
+
+
+class Student < Person
+
+  def initialize(name)
+    super(name)
+  end
+
+  def greeting
+    puts "Hi, my name is #{@name}"
+  end
+
+  def learn
+    puts "I get it!!"
+  end
+
+
+
+end
+
+class Instructor < Person
+  def initialize(name)
+    super(name)
+
+  end
+
+  def teach
+    puts "Everything in Ruby in an Object!"
+  end
+
+
+end
